52f56e23ad4e0753396851de0685e3d0803e38f1
LordWelheor <Lord_Welheor@mail.ru>
2017-10-29 23:12:37 +0300

    Add JavaScript and remake radiobuttons

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,32 @@
-// Your code here...
+const productForm = document.getElementById('productForm');
 
+productForm.addEventListener('click', function (elem) {
+    const previewImg = document.getElementById('previewImg');
+    const colorName = document.getElementById('colorName');
+    const sizeName = document.getElementById('sizeName');
+    const action = elem.target.getAttribute('data-action');
+
+    switch (action) {
+    case 'S':
+        sizeName.innerHTML = 'S';
+        break;
+    case 'M':
+        sizeName.innerHTML = 'M';
+        break;
+    case 'L':
+        sizeName.innerHTML = 'L';
+        break;
+    case 'white':
+        previewImg.src = 'img/tshirt_white.jpg';
+        colorName.innerHTML = 'Белый';
+        break;
+    case 'yellow':
+        previewImg.src = 'img/tshirt_yellow.jpg';
+        colorName.innerHTML = 'Желтый';
+        break;
+    case 'green':
+        previewImg.src = 'img/tshirt_green.jpg';
+        colorName.innerHTML = 'Зеленый';
+        break;
+    }
+});
