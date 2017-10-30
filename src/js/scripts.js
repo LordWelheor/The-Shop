3a00edfd8e3e96147df9726336de24cac85697a3
LordWelheor <Lord_Welheor@mail.ru>
2017-10-30 20:42:14 +0300

    Refactor code

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,32 +1,26 @@
 const productForm = document.getElementById('productForm');
+const previewImg = document.getElementById('previewImg');
+const colorName = document.getElementById('colorName');
+const sizeName = document.getElementById('sizeName');
+const size = ['S', 'M', 'L'];
+const color = [
+    {nameEng: 'white', nameRu: 'Белый'},
+    {nameEng: 'yellow', nameRu: 'Желтый'},
+    {nameEng: 'green', nameRu: 'Зеленый'}
+];
 
 productForm.addEventListener('click', function (elem) {
-    const previewImg = document.getElementById('previewImg');
-    const colorName = document.getElementById('colorName');
-    const sizeName = document.getElementById('sizeName');
     const action = elem.target.getAttribute('data-action');
 
-    switch (action) {
-    case 'S':
-        sizeName.innerHTML = 'S';
-        break;
-    case 'M':
-        sizeName.innerHTML = 'M';
-        break;
-    case 'L':
-        sizeName.innerHTML = 'L';
-        break;
-    case 'white':
-        previewImg.src = 'img/tshirt_white.jpg';
-        colorName.innerHTML = 'Белый';
-        break;
-    case 'yellow':
-        previewImg.src = 'img/tshirt_yellow.jpg';
-        colorName.innerHTML = 'Желтый';
-        break;
-    case 'green':
-        previewImg.src = 'img/tshirt_green.jpg';
-        colorName.innerHTML = 'Зеленый';
-        break;
-    }
+    size.forEach( el => {
+        if (action === el)
+            sizeName.innerHTML = el;
+    });
+
+    color.forEach( el => {
+        if (action === el.nameEng) {
+            previewImg.src = 'img/tshirt_' + el.nameEng + '.jpg';
+            colorName.innerHTML = el.nameRu;
+        }
+    });
 });
