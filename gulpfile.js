52c55e801aec2152ac5d3c78a8b14e704601a429
LordWelheor <Lord_Welheor@mail.ru>
2017-11-04 03:33:34 +0300

    Add data.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
-const gulp = require('gulp');
-const less = require('gulp-less');
-const browserSync = require('browser-sync');
-const autoprefixer = require('gulp-autoprefixer');
-const rename = require('gulp-rename');
-const ejs = require('gulp-ejs');
-const gutil = require('gulp-util');
-const sourcemaps = require('gulp-sourcemaps');
-const include = require('gulp-include');
-const imagemin = require('gulp-imagemin');
-const gulpIf = require('gulp-if');
+const gulp          = require('gulp');
+const less          = require('gulp-less');
+const browserSync   = require('browser-sync');
+const autoprefixer  = require('gulp-autoprefixer');
+const rename        = require('gulp-rename');
+const ejs           = require('gulp-ejs');
+const gutil         = require('gulp-util');
+const sourcemaps    = require('gulp-sourcemaps');
+const include       = require('gulp-include');
+const imagemin      = require('gulp-imagemin');
+const gulpIf        = require('gulp-if');
+const data          = require('./src/data');
 
 const env = process.env.NODE_ENV;
 const isDevelopment = !env || env === 'development';
@@ -26,6 +27,23 @@ gulp.task('livereload', () => {
             'dist/**/*.*'
         ]
     });
+});
+
+// Отслеживание изменений в файлах, нужно только при локальной разработке
+gulp.task('watch', () => {
+    gulp.watch('src/less/**/*.less', ['styles']);
+    gulp.watch('src/**/*.html', ['html']);
+    gulp.watch('src/**/*.ejs', ['html']);
+    gulp.watch('src/img/**/*.*', ['img']);
+    gulp.watch('src/js/**/*.*', ['js']);
+});
+
+gulp.task('html', () => {
+    gulp.src('src/index.ejs')
+        .pipe(ejs(data).on('error', gutil.log))
+        .pipe(include())
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('styles', () => {
@@ -46,23 +64,6 @@ gulp.task('img', () => {
 gulp.task('js', () => {
     gulp.src('src/js/**/*.*')
         .pipe(gulp.dest('./dist/js'));
-});
-
-gulp.task('html', () => {
-    gulp.src('src/index.ejs')
-        .pipe(ejs().on('error', gutil.log))
-        .pipe(include())
-        .pipe(rename('index.html'))
-        .pipe(gulp.dest('./dist'));
-});
-
-// Отслеживание изменений в файлах, нужно только при локальной разработке
-gulp.task('watch', () => {
-    gulp.watch('src/less/**/*.less', ['styles']);
-    gulp.watch('src/**/*.html', ['html']);
-    gulp.watch('src/**/*.ejs', ['html']);
-    gulp.watch('src/img/**/*.*', ['img']);
-    gulp.watch('src/js/**/*.*', ['js']);
 });
 
 gulp.task('default', ['styles', 'html', 'img', 'js', 'livereload', 'watch']);
---
+++ src/data.js
@@ -0,0 +1,19 @@
+module.exports = {
+    nav: ['LISP', 'OCaml', 'JavaScript', 'Python', 'PHP'],
+    breadcrumbs: ['LISP', 'Одежда', 'Футболки'],
+    size: ['S', 'M', 'L'],
+    color: [
+        {name: 'white', value: '#fff'},
+        {name: 'yellow', value: '#e3d954'},
+        {name: 'green', value: '#a5ae96'}
+    ],
+    support: ['Как сделать заказ', 'Возврат товара', 'Публичная офёрта', 'Доставка', 'Способы оплаты',
+        'Правила продажи', 'Возврат денежных средств', 'Вопросы и ответы'],
+    about: ['О нас', 'Контакты', 'Партнёрам', 'Вакансии', 'Сертификаты', 'Преимущества', 'Наши скидки'],
+    services: [
+        {svg: 'delivery', text: 'бесплатная доставка от 4000 ₽'},
+        {svg: 'heart', text: 'пробники в подарок при каждой покупке'},
+        {svg: 'cup', text: 'акции и подарки за покупку'}
+    ],
+    social: ['facebook', 'twitter', 'instagram']
+};
